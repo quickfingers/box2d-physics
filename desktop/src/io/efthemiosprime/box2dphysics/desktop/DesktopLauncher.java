d4ed2043dbc5331db240d5e533d781d6c59f88c8
Efthemios Suyat <bongbox@gmail.com>
2015-11-15 20:00:58 -0500

    project setup

---
+++ desktop/src/io/efthemiosprime/box2dphysics/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package io.efthemiosprime.box2dphysics.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import io.efthemiosprime.box2dphysics.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
